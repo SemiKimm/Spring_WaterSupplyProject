05f8f2dd09410e5810593112ebaffd231ed63dcb
김세미 <semikimm.m@gmail.com>
2022-04-16 21:02:13 +0900

    [spring-core-final-test] 요금표저장소테스트 - 구간별 표금 찾기 구현 위해서 WaterRate 속성 추가 2

--- springframework-finaltest-kimsemi/src/main/java/com/nhnacademy/edu/springframework/project/domain/WaterRate.java
+++ springframework-finaltest-kimsemi/src/main/java/com/nhnacademy/edu/springframework/project/domain/WaterRate.java
@@ -6,6 +6,9 @@ public class WaterRate {
     private final int number;
     private final String city;
     private final String sector;
+    private final long level;
+    private final long unitStart;
+    private final long unitEnd;
     private final long unitPrice;
 
     public WaterRate(int number, String city, String sector, long level, long unitStart,
@@ -13,6 +16,9 @@ public class WaterRate {
         this.number = number;
         this.city = city;
         this.sector = sector;
+        this.level = level;
+        this.unitStart = unitStart;
+        this.unitEnd = unitEnd;
         this.unitPrice = unitPrice;
     }
 
@@ -29,14 +35,15 @@ public class WaterRate {
             return false;
         }
         WaterRate waterRate = (WaterRate) o;
-        return number == waterRate.number && unitPrice == waterRate.unitPrice &&
-            Objects.equals(city, waterRate.city) &&
+        return number == waterRate.number && level == waterRate.level &&
+            unitStart == waterRate.unitStart && unitEnd == waterRate.unitEnd &&
+            unitPrice == waterRate.unitPrice && Objects.equals(city, waterRate.city) &&
             Objects.equals(sector, waterRate.sector);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(number, city, sector, unitPrice);
+        return Objects.hash(number, city, sector, level, unitStart, unitEnd, unitPrice);
     }
 
     @Override
@@ -45,6 +52,9 @@ public class WaterRate {
             "number=" + number +
             ", city='" + city + '\'' +
             ", sector='" + sector + '\'' +
+            ", level=" + level +
+            ", unitStart=" + unitStart +
+            ", unitEnd=" + unitEnd +
             ", unitPrice=" + unitPrice +
             '}';
     }
