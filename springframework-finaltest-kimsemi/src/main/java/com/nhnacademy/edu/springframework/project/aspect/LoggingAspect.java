5fa62b34d40fd29528b7b1033cf6ddc9a12fe88b
김세미 <semikimm.m@gmail.com>
2022-04-17 01:12:11 +0900

    [spring-core-final-test] 메소드 실행시간 elapse.log 파일 저장(AOP 이용) - 완료

---
+++ springframework-finaltest-kimsemi/src/main/java/com/nhnacademy/edu/springframework/project/aspect/LoggingAspect.java
@@ -0,0 +1,26 @@
+package com.nhnacademy.edu.springframework.project.aspect;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private final Log log = LogFactory.getLog(LoggingAspect.class);
+    @Around("execution(* *(..))")
+    public Object logTimer(ProceedingJoinPoint ptj) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        try{
+            stopWatch.start(ptj.getSignature().getName());
+            return ptj.proceed();
+        }finally {
+            stopWatch.stop();
+            log.info(ptj.getSignature().getName()+" : "+stopWatch.prettyPrint());
+        }
+    }
+}
